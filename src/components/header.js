4d00864285b2e4ef441f8f510f8cf196def09f4d
axecopfire <sbraun990@gmail.com>
2019-07-09 17:37:09 -0400

    started dev with react

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from '../images/logo.jpg';
+
+export default function () {
+    return (
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1>Schuster Braun's Website</h1>
+      </header>
+    )
+}
