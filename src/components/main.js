9517eefd6543049630b0c1ad00573bab1ea1a28b
axecopfire <sbraun990@gmail.com>
2019-07-13 11:53:32 -0400

    basic content added

--- src/components/main.js
+++ src/components/main.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import SoDev from '../images/SoDev-logo.png';
+import Teaching from '../images/teaching.jpg';
 
 export default function () {
     return (
-        <h1>Schuster Braun's Website</h1>
+        <main>
+            <h1>Schuster Braun</h1>
+            <h2>I am a web developer, teacher, community advocate, VetsWhoCode alumni, Navy veteran.</h2>
+            <section className="SouthernDev">
+                <img src={SoDev} alt="Cool logo for Southern Dev Conference. Stylized url: SouthernDev.co" />
+                <p>Community Advocate: Southern Dev conference and the monthly Developer Meetups.</p>
+            </section>
+            <section className="teaching">
+                <div className="orange-triangle">
+                </div>
+                <p>Teacher: theClubhouse’s Code Boot Camp</p>
+                <img src={Teaching} alt="I'm pointing out some broken code a student has." />
+            </section>
+        </main>
     )
 }
