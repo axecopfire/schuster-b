c97761555e3d0f50e8089102a098d01d47b992b2
axecopfire <sbraun990@gmail.com>
2019-07-21 18:00:42 -0400

    changing words

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,20 +6,20 @@ export default function () {
     return (
         <main>
             <h1>Schuster Braun</h1>
-            <h2>I am a web developer, teacher, community advocate, VetsWhoCode alumni, Navy veteran.</h2>
+            <h2>Community driven Web Developer, passionate about building cool stuff for the web, and educating the next generation.</h2>
             <section className="SouthernDev">
                 <div className="image">
                     <a href="https://southerndev.co">
                         <img src={SoDev} alt="Cool logo for Southern Dev Conference. Stylized url: SouthernDev.co" />
                     </a>
                 </div>
-                <p>Community Advocate: Southern Dev conference and the monthly Developer Meetups.</p>
+                <p>Organize Southern Dev conference and monthly Developer Meetups.</p>
             </section>
             <section className="teaching">
                 <div className="leftContainer">
                     <div className="orangeTriangle">
                     </div>
-                    <p>Teacher: theClubhouse’s Code Boot Camp</p>
+                    <p>Teach theClubhouse’s Code Boot Camp</p>
                 </div>
                 <div className="image">
                 <img src={Teaching} alt="I'm pointing out some broken code a student has." />
